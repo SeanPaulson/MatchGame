83b8a7fd07c3747541a53b7993b277071662d302
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-10-02 23:11:09 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<String> animalEmoji = new List<String>()
+            {
+                "🐻‍","🐻‍",
+                "👽","👽",
+                    "🐩","🐩",
+                    "🐻","🐻",
+                    "🐗","🐗",
+                    "🐒","🐒",
+                    "🐒","🐒",
+                    "🐎","🐎",
+
+
+            };
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                Console.WriteLine(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
